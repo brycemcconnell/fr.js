1e443a0f3629c6ee17a6fb6acd639ac7c5e447ca
Bryce McConnell <32624581+brycemcconnell@users.noreply.github.com>
2020-02-20 12:03:22 +1100

    Update storage.mjs

--- storage.mjs
+++ storage.mjs
@@ -6,3 +6,21 @@ export function get(key) {
 export function set(key, value) {
   localStorage.setItem(key, JSON.stringify(value));
 }
+
+export function merge(key, value, duplicates = false) {
+  let original = fr.storage.get(key);
+  let merged;
+  if (!original) {
+    merged = value;
+  } else if (original instanceof Array) {
+    merged = original;
+    if (duplicates) {
+      merged = merged.concat(value);
+    } else {
+      merged = [...new Set([...merged, ...value])];
+    }
+  } else if (original instanceof Array == false) {
+    merged = Object.assign(original, value);
+  }
+  fr.storage.set(key, merged);
+}
