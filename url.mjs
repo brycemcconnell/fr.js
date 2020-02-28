61113cf47c109d4160fcd0ff5701c81ffacf462e
Bryce McConnell <32624581+brycemcconnell@users.noreply.github.com>
2020-02-28 14:20:04 +1100

    Create url.mjs

---
+++ url.mjs
@@ -0,0 +1,21 @@
+export function params() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return Object.fromEntries([...urlParams.entries()]);
+},
+export function query() {
+  const urlParams = new URLSearchParams(window.location.search);
+  let query = urlParams ? '?' + urlParams.toString() : '';
+  return query;
+},
+export function modify(key, value) {
+let searchParams = new URLSearchParams(window.location.search);
+  searchParams.set(key, value);
+  let newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
+  window.history.pushState(null, '', newRelativePathQuery);
+},
+export function unset(key) {
+  let searchParams = new URLSearchParams(window.location.search);
+  searchParams.delete(key);
+  let newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
+  window.history.pushState(null, '', newRelativePathQuery);
+}
